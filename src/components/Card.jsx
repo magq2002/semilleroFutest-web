de63cce29ea4ee305cdb56b20dbb31adb709b76c
Johan <joanalexander337@gmail.com>
2025-03-17 17:56:03 -0500

    cambie color de cartas

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,11 +5,11 @@ export default function Card(props) {
     return (
         <div className={`relative flex flex-col min-w-0 break-words w-full mb-8 shadow-lg rounded-lg ${bgClass}`}>
             <div className="px-4 py-5 flex-auto">
-                <div className={`text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full ${bgCircle}`}>
+                <div className={`text-white p-3 text-left inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full ${bgCircle}`}>
                     <i className="fas fa-award"></i>
                 </div>
                 <h6 className="text-xl font-semibold">{cardName}</h6>
-                <p className="mt-2 mb-4 text-gray-600">
+                <p className="mt-2 mb-4 text-orange-600">
                     {props.children}
                 </p>
             </div>
